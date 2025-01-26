44b3408cb0500e46d258a9806bc8f7ee1b9a478b
yernstee <seannatt@yahoo.com>
2025-01-25 22:19:52 -0800

    Add files via upload

---
+++ phisher.py
@@ -0,0 +1,137 @@
+login-form {
+ background-color: #f5f5f5;
+ padding: 20px;
+ }
+ .logo {
+ width: 160px;
+ height: 70px;
+ float: left;
+ margin-left: 30px;
+ }
+ .user-name {
+ display: inline-block;
+ font-size: 24px;
+ padding-bottom: 15px;
+ }
+ .password {
+ margin-top: 15px;
+ width: 280px;
+ height: 40px;
+ font-size: 20px;
+ }
+ input[type="text"] {
+ border: none;
+ color: #333;
+ background-color: #fff;
+ padding: 15px;
+ width: 180px;}
+ input[type="submit"] {
+ float: right;
+ font-size: 24px;
+ margin-top: 10px;
+ color: #333;
+ background-color: #fff;
+ padding: 15px;
+ width: 90px;
+ }
+ input[type="submit"]:hover {
+ background-color: #4c8f7d;
+ color: #fff;
+ }
+ .button {
+ float: right;
+ margin-top: 30px;
+ font-size: 20px;
+ line-height: 1.5em;
+ padding: 15px;
+ width: 280px;
+ background-color: #4c8f7d;
+ color: #fff;
+ }
+ .button:hover {
+ background-color: #3b6099;
+ color: #fff;
+ }
+ input[type="reset"] {
+ margin-top: 15px;
+ width: 280px;
+ height: 40px;
+ font-size: 20px;
+ }
+ .confirm {
+ margin-left: 30px;
+ font-weight: bold;
+ font-size: 16px;
+ color: #7a7a7a;
+ padding-bottom: 5px;
+ }
+ 
+```
+CSS Code:
+```
+.login-form {
+ background-color: #f5f5f5;
+ padding: 20px;
+ }
+ .logo {
+ width: 160px;
+ height: 70px;
+ float: left;
+ margin-left: 30px;
+ }
+ input[type="text"] {
+ border: none;
+ color: #333;
+ background-color: #fff;
+ padding: 15px;
+ width: 180px;
+ }
+ .password {
+ float: right;
+ margin-top: 15px;
+ width: 280px;
+ height: 40px;
+ font-size: 20px;
+ }
+ input[type="submit"] {
+ float: right;
+ font-size: 24px;
+ margin-top: 10px;
+ color: #333;
+ background-color: #fff;
+ padding: 15px;
+ width: 90px;
+ }
+ input[type="submit"]:hover {
+ background-color: #4c8f7d;
+ color: #fff;
+ }
+ .button {
+ float: right;
+ margin-top: 30px;
+ font-size: 20px;
+ line-height: 1.5em;
+ padding: 15px;
+ width: 280px;
+ background-color: #4c8f7d;
+ color: #fff;
+ }
+ .button:hover {
+ background-color: #3b6099;
+ color: #fff;
+ }
+ input[type="reset"] {
+ margin-top: 15px;
+ width: 280px;
+ height: 40px;
+ font-size: 20px;
+ }
+ .confirm {
+ margin-left: 30px;
+ font-weight: bold;
+ font-size: 16px;
+ color: #7a7a7a;
+ padding-bottom: 5px;
+ }
+
+
